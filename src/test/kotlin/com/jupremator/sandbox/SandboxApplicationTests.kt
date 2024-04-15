0829534537111151ae462b0676803663f8150f6e
oabdelnour <olivier.abdelnour@gmail.com>
2024-04-16 00:19:07 +0200

    init

---
+++ src/test/kotlin/com/jupremator/sandbox/SandboxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jupremator.sandbox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SandboxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
